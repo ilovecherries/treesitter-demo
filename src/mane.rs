bfb7662cca4a2884a6d19bb219ddc8105b17ce31
ilovecherries <darienreese@gmail.com>
2021-04-08 20:15:35 -0400

    ok something builds

--- src/mane.rs
+++ src/mane.rs
@@ -1,18 +1,7 @@
 use tree_sitter::{Parser, Language};
-use std::path::PathBuf;
-
-extern "C" { fn tree_sitter_javascript() -> Language; }
 
 fn main() {
-    // get directory of treesitter backend
-    let dir: PathBuf = ["tree-sitter-javascript", "src"].iter().collect();
-
-    // build the treesitter backend
-    cc::Build::new()
-        .include(&dir)
-        .file(dir.join("parser.c"))
-        .file(dir.join("scanner.c"))
-        .compile("tree-sitter-javascript");
+    extern "C" { fn tree_sitter_javascript() -> Language; }
 
     // get a parser from one of the languages
     let language = unsafe { tree_sitter_javascript() };
@@ -25,6 +14,4 @@ fn main() {
     let root_node = tree.root_node();
 
     println!("{}", root_node.to_sexp());
-
-    println!("Hello, world!");
 }
